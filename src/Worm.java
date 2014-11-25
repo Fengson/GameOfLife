fed753b8ad16e66e16e5d24c0f28735f96363497
Naxster <konto.xbox36@gmail.com>
2014-11-25 20:08:12 +0100

    Zmiana wielu elementów klasy Worm. dodanie dziedziczenia z przeskokiem i ciagłosci zmian genetycznych.

--- src/Worm.java
+++ src/Worm.java
@@ -37,16 +37,43 @@
 
 public class Worm extends Hex {
 
-    private int[] gene;
+    private int[] Gene;
+    private int[] inheritedGene;
     private HexDirection direction;
     private int mass;
     private final int WEIGHT_LOSS_PER_ROUND = 1;
 
     public Worm(int x, int y, int mass) {
         super(x, y);
-        gene = new int[getDirectionsNumber()];
-        direction = getRandomDirection();
+        this.Gene = new int[getDirectionsNumber()];
+        this.inheritedGene = new int[getDirectionsNumber()];
+        for(int i=0; i<getDirectionsNumber(); i++) {
+            Gene[i] = getRandomGeneValue();
+            inheritedGene[i] = Gene[i];
+        }
+        this.direction = getRandomDirection();
         this.mass = mass;
+    }
+
+    public Worm(int x, int y, Worm parent) {
+        super(x, y);
+        this.direction = parent.getDirection();
+        this.mass = (int)(0.5*parent.getMass());
+        this.inheritedGene = parent.getGenes();
+
+        this.Gene = new int[getDirectionsNumber()];
+        int[] grandpaGenes = parent.getInheritedGenes();
+        int diff = 0;
+
+        for(int i=0; i<getDirectionsNumber(); i++) {
+            diff = inheritedGene[i] - grandpaGenes[i];
+            Gene[i] = inheritedGene[i] + (int)(diff*getRandomPercent());
+            if(Gene[i]<0)
+                Gene[i] = 0;
+            else if(Gene[i]>50)
+                Gene[i] = 50;
+        }
+        mutate();
     }
 
     /**
@@ -66,23 +93,26 @@ public class Worm extends Hex {
          * calculate probabilities
          */
         double[] probability = new double[getDirectionsNumber()];
-        int sum;
+        int sum = 0;
+        for (int j = 0; j < probability.length; j++) {
+            sum += Math.exp(-1 * Gene[j]);
+        }
         for (int i = 0; i < probability.length; i++) {
-            sum = 0;
-            for (int j = 0; j < probability.length; j++) {
-                sum += Math.exp(-1 * gene[j]);
-            }
-            probability[i] = Math.exp(-1 * gene[i]) / sum;
+            probability[i] = Math.exp(-1 * Gene[i]) / sum;
         }
 
         /**
          * pick index based on probabilities
          */
-        double rand = Math.random() * 100;
         double probabilitiesSum = 0;
-        for (int i = 0; i < probability.length; i++) {
+        double tmp = 0;
+        for (int i = 0; i < probability.length; i++)
             probabilitiesSum += probability[i];
-            if (rand < probabilitiesSum) {
+        double rand = Math.random() * probabilitiesSum;
+
+        for (int i = 0; i < probability.length; i++) {
+            tmp += probability[i];
+            if (rand < tmp) {
                 return direction = HexDirection.values()[i];
             }
         }
@@ -92,7 +122,12 @@ public class Worm extends Hex {
 
 
     public void mutate() {
-        gene[getRandomDirectionIndex()] = getRandomGeneValue();
+        int i = getRandomDirectionIndex();
+        Gene[i] += getRandomGeneMod();
+        if(Gene[i]<0)
+            Gene[i] = 0;
+        else if(Gene[i]>50)
+            Gene[i] = 50;
     }
 
     public int getMass() {
@@ -103,12 +138,32 @@ public class Worm extends Hex {
         this.mass = mass;
     }
 
+    public HexDirection getDirection() {
+        return direction;
+    }
+
+    public int[] getGenes() {
+        return Gene;
+    }
+
+    public int[] getInheritedGenes() {
+        return inheritedGene;
+    }
+
     private HexDirection getRandomDirection() {
         return HexDirection.values()[getRandomDirectionIndex()];
     }
 
     private int getRandomGeneValue() {
-        return (int) (Math.random() * 100);
+        return (int) (Math.random() * 51);
+    }
+
+    private int getRandomGeneMod() {
+        return (int) (-10 + Math.random() * 21);
+    }
+
+    private float getRandomPercent() {
+        return ( (int)(-100 + Math.random() * 301) )/100;
     }
 
     private int getRandomDirectionIndex() {
